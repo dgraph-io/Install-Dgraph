22375b4e912fa82c79cbcf428ae6b29f37781c6c
Pawan Rawal <pawan@dgraph.io>
2017-01-19 15:36:22 +1100

    Trapping and displaying error on non-zero exit codes

--- getdgraph.sh
+++ getdgraph.sh
@@ -179,4 +179,11 @@ print_error "
 	print_instruction "Please visit https://wiki.dgraph.io/Get_Started for further instructions on usage."
 }
 
+function exit_error {
+  if [ "$?" -ne 0 ]; then
+    print_error "There was some problem while installing Dgraph. Please share the output of this script with us on https://dgraph.slack.com or https://discuss.dgraph.io so that we can resolve the issue for you."
+  fi
+}
+
+trap exit_error EXIT
 install_dgraph "$@"
