2f2c965ae583b19fac136608dc90c8d33f09ff87
Janardhan Reddy <janardhan@dgraph.io>
2018-01-17 16:24:06 +1100

    Check chechsum after downloading file

--- getdgraph.sh
+++ getdgraph.sh
@@ -113,15 +113,6 @@ printf $RESET
 		tar_file=dgraph-$platform-amd64-$release_version".tar.gz"
 		dgraph_link="https://github.com/dgraph-io/dgraph/releases/download/"$release_version"/"$tar_file
 
-		# Backup existing dgraph binaries in HOME directory
-		if hash dgraph 2>/dev/null; then
-			dgraph_path="$(which dgraph)"
-			dgraph_backup="dgraph_backup_olderversion"
-			print_step "Backing up older versions in ~/$dgraph_backup (password may be required)."
-			mkdir -p ~/$dgraph_backup
-			$sudo_cmd mv $dgraph_path* ~/$dgraph_backup/.
-		fi
-
 		# Download and untar Dgraph binaries
 		if curl --output /dev/null --silent --head --fail "$dgraph_link"; then
 			print_step "Downloading $dgraph_link"
@@ -134,8 +125,27 @@ printf $RESET
 		fi
 
 		print_step "Inflating binaries (password may be required).";
-		$sudo_cmd tar -C /usr/local/bin -xzf /tmp/$tar_file;
+		temp_dir=$(mktemp -d)
+		tar -C $temp_dir -xzf /tmp/$tar_file
+		dgraphsum=$($digest_cmd $temp_dir/dgraph | awk '{print $1;}')
+		if [ "$dgraph" != "$dgraphsum" ]; then
+      print_error "Downloaded binaries checksum doesn't match with latest versions checksum"
+			echo
+			exit 1;
+		fi
+
+		# Backup existing dgraph binaries in HOME directory
+		if hash dgraph 2>/dev/null; then
+			dgraph_path="$(which dgraph)"
+			dgraph_backup="dgraph_backup_olderversion"
+			print_step "Backing up older versions in ~/$dgraph_backup (password may be required)."
+			mkdir -p ~/$dgraph_backup
+			$sudo_cmd mv $dgraph_path* ~/$dgraph_backup/.
+		fi
+
+		$sudo_cmd mv $temp_dir/* /usr/local/bin/.
 		rm "/tmp/"$tar_file;
+		rm -rf $temp_dir
 
 		# Check installation
 		if hash dgraph 2>/dev/null; then
