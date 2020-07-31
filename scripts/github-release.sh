8331a716affe5b435943de37f53f93e1d11c54c9
Daniel Mai <daniel@dgraph.io>
2020-07-31 11:37:51 -0700

    fix(github-release): Set the latest release version explicitly.
    GitHub release's "latest" version always points to the latest tag,
    which isn't necessarily the latest Dgraph release. For example, we can
    release v1.2.x, but v20.07.x is still the latest release series.

--- scripts/github-release.sh
+++ scripts/github-release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+LATEST_RELEASE="v20.07.0"
+
 update_latest_release() {
   response=$(curl --write-out "%{http_code}" --silent --output /tmp/latest-release.txt \
-  https://api.github.com/repos/dgraph-io/dgraph/releases/latest)
+  https://api.github.com/repos/dgraph-io/dgraph/releases/tags/$LATEST_RELEASE)
 
   if [ "$response" == "200" ]; then
     mv /tmp/latest-release.txt latest-release.txt
