272977db7473de49081760a6447e036d293d344f
Sudhish <108091997+skrdgraph@users.noreply.github.com>
2023-01-07 16:46:45 +0000

    fix install scripts for v22+ (#33)
    * bug fix in this checksum file
    
    * fix buggy install script
    
    * add comments
    
    * Update getdgraph.sh
    
    * Update .gitignore
    
    * Update getdgraph.sh
    
    * Update getdgraph.sh

--- getdgraph.sh
+++ getdgraph.sh
@@ -173,7 +173,16 @@ printf "%b" "$RESET"
 		exit 1;
 	fi
 
-	dgraph=$(grep -m 1 "$install_path"/dgraph  /tmp/"$checksum_file" | grep -E -o '[a-zA-Z0-9]{64}')
+	if grep -Fxq "$install_path"/dgraph /tmp/"$checksum_file"
+	then
+		# for versions < 22, the install path is present in the SHA file
+		# example file from release v21.03.2 https://github.com/dgraph-io/dgraph/releases/download/v21.03.2/dgraph-checksum-linux-amd64.sha256
+		dgraph=$(grep -m 1 "$install_path"/dgraph  /tmp/"$checksum_file" | grep -E -o '[a-zA-Z0-9]{64}')
+	else
+		# for versions >= 22, the install path is NOT present in the SHA file
+		# example file from release v22.0.2 https://github.com/dgraph-io/dgraph/releases/download/v22.0.2/dgraph-checksum-linux-amd64.sha256
+		dgraph=$(cat /tmp/"$checksum_file")
+	fi
 
 	if [ "$dgraph" == "" ]; then
 	     print_error "Sorry, we don't have binaries for this platform. Please build from source."
